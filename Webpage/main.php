1ac0990e8d948c19a44b1caa2907b38bdd1349b7
LINHSINJU <gogo.3164.owqjsu@gmail.com>
2024-10-06 22:34:48 +0800

    Add main.php in Webpage file.

---
+++ Webpage/main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" title="藝術家的藏寶庫">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>藝術家的藏寶庫</title>
+    <style>
+        #head{
+            height: 100vh;
+            background-color: beige;
+        }
+        #head h1{
+            text-align: center;
+            color: chocolate;
+        }
+    </style>
+</head>
+<body>
+    <div id="head">
+        <h1>歡迎來到藝術家的藏寶庫！</h1>
+    </div>
+</body>
+</html>
